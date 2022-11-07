a3ff74350a3882363b6ce18ab2853153a7fa8fcb
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2022-11-07 22:50:13 +0530

    Update Binary.cpp

--- Day0020 Converted Array  to Binary Search Array/Binary.cpp
+++ Day0020 Converted Array  to Binary Search Array/Binary.cpp
@@ -1,24 +1,24 @@
-Topic:Array
-SubTopic:Converted Array to Binary Search Array
-Description:Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+📌Topic:Array
+📌SubTopic:Converted Array to Binary Search Array
+📌Description:Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
 
- TestCase 1:
+📌 TestCase 1:
 Input: nums = [-10,-3,0,5,9]
 Output: [0,-3,9,-10,null,5]
 Explanation: [0,-10,5,null,-3,null,9] is also accepted:
 
-TestCase 2:
+📌TestCase 2:
 Input: nums = [1,3]
 Output: [3,1]
 Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
  
 
-Constraints:
+📌Constraints:
 1 <= nums.length <= 10^4
 -10^4 <= nums[i] <= 10^4
 nums is sorted in a strictly increasing order.
   
-  Code:
+ 📌Code:
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
 
