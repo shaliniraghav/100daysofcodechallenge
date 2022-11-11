460746b15ecba84f2921f61d7606e253aae50484
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2022-11-11 23:45:41 +0530

    Day 24 Progress
    📌Description:Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
    You must implement a solution with a linear runtime complexity and use only constant extra space.
    
    📌TestCase 1:
    Input: nums = [2,2,1]
    Output: 1
    
    📌TestCase 2:
    Input: nums = [4,1,2,1,2]
    Output: 4
    
    📌TestCase 3:
    Input: nums = [1]
    Output: 1
     
    
    📌Constraints:
    1 <= nums.length <= 3 * 104
    -3 * 104 <= nums[i] <= 3 * 104
    Each element in the array appears twice except for one element which appears only once.

---
+++ Day0024 Single Number/Number.java
@@ -0,0 +1,37 @@
+📌Topic:Array
+📌SubTopic:Single Number
+📌Description:Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+📌TestCase 1:
+Input: nums = [2,2,1]
+Output: 1
+
+📌TestCase 2:
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+📌TestCase 3:
+Input: nums = [1]
+Output: 1
+ 
+
+📌Constraints:
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+  
+📌Code:
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length == 1) 
+            return nums[0];
+    for (int i = 1; i < nums.length; i += 2) {
+            if (nums[i] != nums[i - 1]) {
+                return nums[i - 1];
+            }
+        }
+   return nums[nums.length - 1];
+    }
+}  
