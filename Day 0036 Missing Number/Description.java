5ec87d5ce7e69db2fb6fa50a7d7ee12272abd1a8
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2022-11-27 11:44:49 +0530

    Day 36 Progress
    📌TestCase 1:
    
    Input: nums = [3,0,1]
    Output: 2
    Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums. 
      
    📌TestCase 2:
    
    Input: nums = [0,1]
    Output: 2
    Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
    
    📌TestCase 3:
    
    Input: nums = [9,6,4,2,3,5,7,0,1]
    Output: 8
    Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
     
    
    📌Constraints:
    
    n == nums.length
    1 <= n <= 104
    0 <= nums[i] <= n
    All the numbers of nums are unique.

---
+++ Day 0036 Missing Number/Description.java
@@ -0,0 +1,43 @@
+📌Topic:Array
+
+📌SubTopic:Missing Number
+
+📌Description:
+Given an array nums containing n distinct numbers in the range [0, n], 
+return the only number in the range that is missing from the array.
+
+📌TestCase 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums. 
+  
+📌TestCase 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+📌TestCase 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+ 
+
+📌Constraints:
+
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.
+  
+📌Code:
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length,totalsum=n*(n+1)/2; //sum of first n natural numbers
+        for(int i:nums)
+            totalsum-=i;
+        return totalsum;
+    }
+}   
