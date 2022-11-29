517497ef1c88f3150db845c34d56c42c168d60c8
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2022-11-29 16:57:09 +0530

    day 38
    📌Topic:Array
    
    📌SubTopic:Intersection Of Two Arrays
    
    📌Description:
    Given two integer arrays nums1 and nums2, return an array of their intersection. 
    Each element in the result must be unique and you may return the result in any order.
    
    📌TestCase 1:
    Input: nums1 = [1,2,2,1], nums2 = [2,2]
    Output: [2]
    
    📌TestCase 2:
    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
    Output: [9,4]
    Explanation: [4,9] is also accepted.
     
    
    📌Constraints:
    1 <= nums1.length, nums2.length <= 1000
    0 <= nums1[i], nums2[i] <= 1000

---
+++ Day 0038 Intersection Of Two Arrays/Description.java
@@ -0,0 +1,44 @@
+📌Topic:Array
+
+📌SubTopic:Intersection Of Two Arrays
+
+📌Description:
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must be unique and you may return the result in any order.
+
+📌TestCase 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+📌TestCase 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ 
+
+📌Constraints:
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+📌Code:
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}   
+    
