b051c4e980d0d2a89d9e7eeec183378009d789d0
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2023-01-20 01:58:10 +0530

    day 59 progress

---
+++ Day 0059 Shortest Completing Word/Description.java
@@ -0,0 +1,64 @@
+📌Topic: Array
+📌Subtopic:Shortest Completing Word
+📌Description:Given a string licensePlate and an array of strings words, find the shortest completing word in words.
+A completing word is a word that contains all the letters in licensePlate. Ignore numbers and spaces in licensePlate, and treat letters as case insensitive. If a letter appears more than once in licensePlate, then it must appear in the word the same number of times or more.
+For example, if licensePlate = "aBc 12c", then it contains letters 'a', 'b' (ignoring case), and 'c' twice. Possible completing words are "abccdef", "caaacab", and "cbca".
+Return the shortest completing word in words. It is guaranteed an answer exists. If there are multiple shortest completing words, return the first one that occurs in words.
+
+ 
+📌Testcase 1:
+Input: licensePlate = "1s3 PSt", words = ["step","steps","stripe","stepple"]
+Output: "steps"
+Explanation: licensePlate contains letters 's', 'p', 's' (ignoring case), and 't'.
+"step" contains 't' and 'p', but only contains 1 's'.
+"steps" contains 't', 'p', and both 's' characters.
+"stripe" is missing an 's'.
+"stepple" is missing an 's'.
+Since "steps" is the only word containing all the letters, that is the answer.
+
+📌Testcase 2:
+Input: licensePlate = "1s3 456", words = ["looks","pest","stew","show"]
+Output: "pest"
+Explanation: licensePlate only contains the letter 's'. All the words contain 's', but among these "pest", "stew", and "show" are shortest. The answer is "pest" because it is the word that appears earliest of the 3.
+ 
+
+📌Constraints:
+1 <= licensePlate.length <= 7
+licensePlate contains digits, letters (uppercase or lowercase), or space ' '.
+1 <= words.length <= 1000
+1 <= words[i].length <= 15
+words[i] consists of lower case English letters.
+  
+📌Code:
+class Solution {
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        String target = licensePlate.toLowerCase();
+        int [] charMap = new int[26];
+        // Construct the character map
+        for(int i = 0 ; i < target.length(); i++){
+            if(Character.isLetter(target.charAt(i))) charMap[target.charAt(i) - 'a']++;
+        }
+        int minLength = Integer.MAX_VALUE;
+        String result = null;
+        for (int i = 0; i < words.length; i++){
+            String word = words[i].toLowerCase();
+            if(matches(word, charMap) && word.length() < minLength) {
+                minLength = word.length();
+                result  = words[i];
+            }
+        }
+        return result;
+    }
+    private boolean matches(String word, int[] charMap){
+        int [] targetMap = new int[26];
+        for(int i = 0; i < word.length(); i++){
+            if(Character.isLetter(word.charAt(i))) targetMap[word.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if(charMap[i]!=0 && targetMap[i]<charMap[i]) return false;
+        }
+        return true;
+    }
+}         
+    
