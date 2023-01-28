ec4accb822de40a37636abbde4e90e799de66de4
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2023-01-29 01:12:22 +0530

    day 61 progress

---
+++ Day 0061 Find  Smallest Letter Greater Than Target/Description.java
@@ -0,0 +1,47 @@
+📌Topic:Array
+📌Subtopic:Find Smallest Letter Greater Than Target
+📌Description:
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+📌Testcase 1:
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+📌Testcase 2:
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+
+📌Testcase 3:
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+ 
+
+📌Constraints:
+2 <= letters.length <= 10^4
+letters[i] is a lowercase English letter.
+letters is sorted in non-decreasing order.
+letters contains at least two different characters.
+target is a lowercase English letter.
+  
+📌Code:
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0, end = letters.length-1; 
+        
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(letters[mid]>target){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        
+        return letters[start % letters.length];
+    }
+}
