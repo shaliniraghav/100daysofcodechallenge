1da489709b99823e15559a36850527d28b12a26f
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2023-02-17 01:37:51 +0530

    Day 80 Progress

---
+++ Day 0080 Multiply String/Description.java
@@ -0,0 +1,36 @@
+topic: String
+Subtopic: Multiply String
+
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+ 
+
+Example 1:
+
+Input: num1 = "2", num2 = "3"
+Output: "6"
+
+Example 2:
+
+Input: num1 = "123", num2 = "456"
+Output: "56088"
+
+ 
+
+Constraints:
+
+    1 <= num1.length, num2.length <= 200
+    num1 and num2 consist of digits only.
+    Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+
+Code: import java.math.BigInteger;
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger a=new BigInteger(num1);
+        BigInteger b=new BigInteger(num2);
+        BigInteger c=a.multiply(b);
+        return c+"";  
+    }
+}
