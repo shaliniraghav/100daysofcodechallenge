34bd38e919600b3b30cde335b790073786ebaf84
Shalini Raghav <114677625+shaliniraghav@users.noreply.github.com>
2023-02-17 11:10:59 +0530

    Day 89 progress

---
+++ Day 0089 Word Break/Description.java
@@ -0,0 +1,42 @@
+Topic:-String
+Subtopic:-Word Break
+Description:-Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+TestCase 1:
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+TestCase 2:
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Testcase 3:
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+Constraints:
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+  
+code:-
+  class Solution {
+     public boolean wordBreak(String s, List<String> wordDict) {
+    Set<String> set = new HashSet<>(wordDict);
+    boolean[] dp = new boolean[s.length() + 1];
+    dp[0] = true;
+    for (int i = 1; i <= s.length(); i++)
+        for (int j = 0; j < i; j++)
+            if (dp[j] && set.contains(s.substring(j, i))) {
+                dp[i] = true;
+                break;
+            }
+    return dp[s.length()];
+}    
+ }
